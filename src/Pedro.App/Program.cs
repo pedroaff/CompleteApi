90ca57a96975057672cfec0674b99838a5ec0c3b
Pedro Faria <pedro.faria@fire-engine-red.com>
2023-11-29 14:42:30 -0300

    setup program with dbcontext. created migrations. add docker

--- src/Pedro.App/Program.cs
+++ src/Pedro.App/Program.cs
@@ -1,25 +1,27 @@
+using Microsoft.EntityFrameworkCore;
+using Pedro.Data.Context;
+
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    var connString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+    builder.Services.AddDbContext<MeuDbContext>(opts => opts.UseMySql(connString, ServerVersion.AutoDetect(connString)));
+
+    builder.Services.AddControllers();
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
 }
+var app = builder.Build();
+{
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
 
-app.UseHttpsRedirection();
+    app.UseHttpsRedirection();
+    app.UseAuthorization();
+    app.MapControllers();
 
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
+    app.Run();
+}
