cf63797ca6cac43aa83ff6b447e7d5f83e594936
Pedro <slain.sviddneger@gmail.com>
2023-12-06 09:12:46 -0300

    generate tokens and return in login and create account actions

--- src/Pedro.App/Configuration/IdentityConfig.cs
+++ src/Pedro.App/Configuration/IdentityConfig.cs
@@ -1,8 +1,11 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using Pedro.App.Data;
 using Pedro.App.Extensions;
 using Pedro.Data.Context;
+using System.Text;
 
 namespace Pedro.App.Configuration;
 
@@ -17,6 +20,34 @@ public static class IdentityConfig
             .AddEntityFrameworkStores<ApplicationDbContext>()
             .AddErrorDescriber<IdentityPtBr>()
             .AddDefaultTokenProviders();
+
+
+        // JWT
+
+        var appSettingsSection = configuration.GetSection("AppSettings");
+        services.Configure<AppSettings>(appSettingsSection);
+
+        var appSettings = appSettingsSection.Get<AppSettings>();
+        var key = Encoding.ASCII.GetBytes(appSettings.Secret);
+
+        services.AddAuthentication(x =>
+        {
+            x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+            x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+        }).AddJwtBearer(x =>
+        {
+            x.RequireHttpsMetadata = false;
+            x.SaveToken = true;
+            x.TokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = new SymmetricSecurityKey(key),
+                ValidateIssuer = true,
+                ValidateAudience = true,
+                ValidAudience = appSettings.ValidoEm,
+                ValidIssuer = appSettings.Emissor,
+            };
+        });
 
         return services;
     }
--- src/Pedro.App/Controllers/AuthController.cs
+++ src/Pedro.App/Controllers/AuthController.cs
@@ -1,8 +1,13 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
 using Pedro.App.DTO;
+using Pedro.App.Extensions;
 using Pedro.Business.Intefaces;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
 
 namespace Pedro.App.Controllers;
 
@@ -13,13 +18,16 @@ public class AuthController : MainController
 
     private readonly SignInManager<IdentityUser> _signInManager;
     private readonly UserManager<IdentityUser> _userManager;
+    private readonly AppSettings _appSettings;
 
     public AuthController(INotificador notificador,
                           SignInManager<IdentityUser> signInManager,
-                          UserManager<IdentityUser> userManager) : base(notificador)
+                          UserManager<IdentityUser> userManager,
+                          IOptions<AppSettings> appSettings) : base(notificador)
     {
         _signInManager = signInManager;
         _userManager = userManager;
+        _appSettings = appSettings.Value;
     }
 
     [HttpPost("criar-conta")]
@@ -40,7 +48,7 @@ public class AuthController : MainController
         {
             await _signInManager.SignInAsync(user, false);
 
-            return CustomResponse(registerUser);
+            return CustomResponse(GerarJwt());
         }
 
         foreach (var error in result.Errors)
@@ -61,7 +69,7 @@ public class AuthController : MainController
                                                               false,
                                                               true);
 
-        if (result.Succeeded) return CustomResponse(loginUser);
+        if (result.Succeeded) return CustomResponse(GerarJwt());
 
         if (result.IsLockedOut)
         {
@@ -72,5 +80,22 @@ public class AuthController : MainController
         NotificarError("Usuário ou senha incorretos");
 
         return CustomResponse(loginUser);
+    }
+
+    private string GerarJwt()
+    {
+        var tokenHandler = new JwtSecurityTokenHandler();
+        var key = Encoding.ASCII.GetBytes(_appSettings.Secret);
+        var token = tokenHandler.CreateToken(new SecurityTokenDescriptor
+        {
+            Issuer = _appSettings.Emissor,
+            Audience = _appSettings.ValidoEm,
+            Expires = DateTime.UtcNow.AddHours(_appSettings.ExpiracaoHoras),
+            SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+        });
+
+        var encoded = tokenHandler.WriteToken(token);
+
+        return encoded;
     }
 }
